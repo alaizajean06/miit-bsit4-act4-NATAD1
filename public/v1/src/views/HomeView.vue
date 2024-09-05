e97949cc1ae1537ae85b85515c093b7cbee1d6e5
alaizajean06 <alaizajeanbacarro06@gmail.com>
2024-09-04 20:13:12 -0800

    new

--- public/v1/src/views/HomeView.vue
+++ public/v1/src/views/HomeView.vue
@@ -1,9 +1,50 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="p-5">
+    <div class="card">
+      <div class=card-header>
+        <h1>Insert Record</h1>
+      </div>
+      <div class=card-body>
+        <table class="table">
+          <tbody>
+            <tr>
+              <td>First name</td>
+              <td><input class="form-control" type="text"></td>
+            </tr>
+            <tr>
+              <td>Last name</td>
+              <td><input class="form-control" type="text"></td>
+
+            </tr>
+            <tr>
+              <td>Age</td>
+              <td><input class="form-control" type="number"></td>
+              
+            </tr>
+            <tr>
+              <td></td>
+              <td><button class="btn btn-primary w-100">submit</button></td>
+            </tr>
+          </tbody>
+        </table>
+    </div>
+    </div>
   </main>
 </template>
+<script lang="ts">
+
+import axios from 'axios';
+
+
+export default {
+  name: "HomeView",
+  methods: {
+    async insertRecord() {
+      await axios.get("");
+
+    }
+  }
+}
+</script>
+
+
